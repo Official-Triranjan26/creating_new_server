c355558c59f75205e8553f7649b30cdffdec7874
Official-Triranjan26 <triranjansamanta@gmail.com>
2022-08-21 23:51:02 +0530

    added package.json and a npm.text file

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Creating Server !!");
+const http = require('http');
+const port=8081;
+
+http
+    .createServer((req, res)=> {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("<h1>Server is listening on port 8081</h1>")
+    res.end();
+    })
+    .listen(port,()=>{
+        console.log(`Nodejs server is stsrted on port ${port}`);
+    });
