2dd8a5742ab0bc102d3264fad24448e358a5a19a
Official-Triranjan26 <triranjansamanta@gmail.com>
2022-08-23 11:48:38 +0530

    installed 3rd party packages

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const http = require('http');
 const port=8081;
 
 http
-    .createServer((req, res)=> {
+    .createServer((req, res)=> {//callback function ,the function means now it is capable of taking requests and sending sesponses 
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write("<h1>Server is listening on port 8081</h1>")
     res.end();
     })
-    .listen(port,()=>{
+    .listen(port,()=>{//callback function
         console.log(`Nodejs server is stsrted on port ${port}`);
     });
