44aefa00d7633162a33862b2c99aceecbcb27c1c
Official-Triranjan26 <triranjansamanta@gmail.com>
2022-08-23 20:38:36 +0530

    creating routs

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const http = require('http');
 const port=8081;
-
+const toDoList=["React","Angular"];
 http
     .createServer((req, res)=> {//callback function ,the function means now it is capable of taking requests and sending sesponses 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("<h1>Server is listening on port 8081</h1>")
+    const {method,url}=req;
+    // console.log(method,url);
+    if(url=="/todos"){
+        if(method=="GET"){
+            res.writeHead(200,{"Content-Type":"text/html"});
+            res.write(toDoList.toString());
+        }
+        else if(method=="POST"){
+            let body="";
+            req
+                .on("error",(err)=>{
+                    console.log(err);
+                })
+                .on("data",(chunk)=>{
+                    body=body+chunk;
+                    console.error(chunk);
+                })
+                .on("end",()=>{
+                    body=JSON.parse(body);
+                    console.log("data :",body);
+                })
+        }
+        else{
+            res.writeHead(501);
+        }
+    }
+    else{
+        res.writeHead(404);
+    }
     res.end();
     })
     .listen(port,()=>{//callback function
